5c8e5dacc6dbc441748f1bdc4b36592d04e7abc0
Mohamed dhiaeddine Hassine <hassine-mohamed.dhiaeddine@isa.utm.md>
2025-02-22 02:53:07 +0200

    001

---
+++ AC/else_Labs/lab2.py
@@ -0,0 +1,10 @@
+#.  1) Program  to  calculate  the  binary,  octal,  and  hexadecimal  equivalent  of  a given natural number entered from the keyboard.
+
+
+
+
+
+# 3)Program that converts an integer from base p to base q (the bases p and q, as well as the digits of the number in base p, are read from the keyboard). 
+
+
+
